2013f4458f6d65ab6910a0c6e09ca034c15ceb10
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-07 16:47:28 +0000

    comparision intro

--- 01_Basics/04_Comparision.js
+++ 01_Basics/04_Comparision.js
@@ -1,1 +1,20 @@
-console.log("usman")
+// console.log("usman")
+// console.log( 2 > 1);
+// console.log( 2 >= 1);
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// strict check ===
+
+console.log("2" === 2); //will check datatype as well
+
+
+
