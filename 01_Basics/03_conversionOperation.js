283e9e0e090fa08f7660dc653cfe916c886d928a
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-07 14:15:33 +0000

    prefix postfix increment assignment

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -26,5 +26,39 @@ Boolean converts as following
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//  ************Operations*********
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 +2);
+// console.log(2 -2);
+// console.log(2 *2);
+// console.log(2 **2);
+// console.log(2 /2);
+// console.log(2 % 2);
+
+
+let str1 = "hello"
+let str2 = " Usman"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 5)* 3 % 2);
+// console.log(+true);
+
+let gameCounter = 100;
+++gameCounter; //prefix ? postfix operator ?
+// assignment study prefix/postfix from mdn! https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment 
+
+console.log(gameCounter);
