2f9b47b539aba0bb4319dd4b115bd350a9acbdb9
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-07 10:44:52 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treats all JS code as newer version.
+
+// alert (3 + 3) // doesnot work we are using nodeJS, not browser.
+
+console.log(3 
+    + 3);
+console.log("usman"); // code readability should be high.
+
+let name = "usman";
+let age = 25;
+let isLoggedIn = true;
+
+//number => 2 to power of 53
+// BigInt
+// string "". ''
+// Boolean => true/false
+// null => standalone value
+// undefined => variable declare krdia hy, lekin value define ni ki hy.
+// symbol => unique components ko identify kerne k liye, React wagera me.
+
+
+// objects
+
+console.log(typeof null); //object type h.
