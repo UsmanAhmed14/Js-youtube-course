be0a79dc6e836cfb5b17f9d6ccf60c9a8aae7014
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-07 13:20:48 +0000

    conversion of data types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,4 +1,30 @@
-let score = 33;
+let score = "Usman Ahmed";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumer = Number(score);
+// console.log(typeof(valueInNumer));
+// console.log(valueInNumer);
+
+// "33" => 33 easliy converted into 33.
+// "33avc" => NaN not a number
+// true => 1.
+// false => 0.
+
+let isLoggedIn = "Usman"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+
+Boolean converts as following
+1 => true; 0 => false
+"" => false; "usman" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
