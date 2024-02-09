51e3e4fb34b25fd70eba5d98076c35566a44d475
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-09 15:36:59 +0000

    studying string

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "usman"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('umerDASH')
+
+console.log(gameName[0]);
