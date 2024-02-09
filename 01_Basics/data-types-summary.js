5c2ce7bacf4496132cb0db98eadc77eb651341f8
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-09 12:25:01 +0000

    will continue after break

---
+++ 01_Basics/data-types-summary.js
@@ -0,0 +1,24 @@
+// primitive data types
+
+// 7 Types: string, Number, Boolean, Null, Undefined(variable declare krdiya hy lekin y decide ni kiya h, value kia daalenge abhi), Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.33;
+
+const isLoggedIn = true
+const outsideTemp = null
+
+let userEmail; // value undefined hogi, bd mein value assign ki jasakti h.
+
+const id = Symbol('123')
+const AnotherId = Symbol('123')
+
+console.log(id === AnotherId);
+
+const bigNumber = 3243525235n
+
+
+
+// Reference Type(Non Primitive)
+
+// Arrays, Objects, Function
