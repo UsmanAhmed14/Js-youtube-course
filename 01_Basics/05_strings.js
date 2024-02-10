ee1a84fe77116212f1788c93f8459ba3d6a6e6ca
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-10 10:51:20 +0000

    string methods, length

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -3,9 +3,9 @@ const repoCount = 50
 
 // console.log(name + repoCount + " value");
 
-console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //also known as string interpolation 🤓
 
-
-const gameName = new String('umerDASH')
-
+// new keyword use karte hain tou objects❗️ use krte hain JS k 
+const gameName = new String('usman')
 console.log(gameName[0]);
+console.log(gameName.__proto__);
