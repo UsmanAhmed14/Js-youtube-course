10352566dbf398da2f8ef0c2fb96b06500d399cc
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-10 11:37:36 +0000

    learned TRIM method in JS

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -6,6 +6,33 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //also known as string interpolation 🤓
 
 // new keyword use karte hain tou objects❗️ use krte hain JS k 
-const gameName = new String('usman')
+const gameName = new String('hitesh-hc')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4) // ❗️ending index not included. 
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4) 
+console.log(anotherString);
+
+// trim method for string 🖥️
+
+const newStringOne = "    hitesh    hc"
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
+
+
+
