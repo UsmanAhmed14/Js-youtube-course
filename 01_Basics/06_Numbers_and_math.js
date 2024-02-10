558540096d41265d9d81da28a1cac02dd629d5a3
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-10 18:35:32 +0000

    Math and numb

--- 01_Basics/06_Numbers_and_math.js
+++ 01_Basics/06_Numbers_and_math.js
@@ -17,10 +17,27 @@ const hundreds = 1000000
 
 // Maths
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.2)); //lowest value lega.
+// console.log(Math.floor(4.2)); //highest value lega.
+// console.log(Math.sqrt(25)); //highest value lega.
+// console.log(Math.pow(8,2)); //highest value lega.
+
+// Random
+
+// const diceThrow1 =Math.random() // hamesha 0 aur 1 k beech ki value ayegi.
+// console.log(diceThrow1);
+// const multiple = (diceThrow1 * 10) + 1
+// console.log(multiple);
 
 
+console.log(Math.random());
+console.log((Math.random()*10)) + 1;
+console.log(Math.floor(Math.random()*10)) + 1;
 
 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
