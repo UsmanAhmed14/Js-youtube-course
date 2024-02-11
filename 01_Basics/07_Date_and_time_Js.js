9c02c056448eb91c7e049caa9e7309889ca2aeaf
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-11 14:42:47 +0000

    01_basics done 🤝

--- 01_Basics/07_Date_and_time_Js.js
+++ 01_Basics/07_Date_and_time_Js.js
@@ -14,5 +14,26 @@ let  myDate = new Date()
 
 
 // let myCreatedDate = new Date(2024, 1, 10) // month starts from 0 in JS
-let myCreatedDate = new Date(2024, 1, 10, 5 ,3) // month starts from 0 in JS
-console.log(myCreatedDate.toLocaleString());
+let myCreatedDate = new Date("2023-01-14") // month starts from 0 in JS
+// console.log(myCreatedDate.toLocaleString());
+
+// TIMESTAMPS
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor((Date.now()/1000)));
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay() + 1);
+
+
+newDate.toLocaleString('default', {
+    // weekday: "long",
+    dateStyle: "short"
+})
+console.log(newDate);
