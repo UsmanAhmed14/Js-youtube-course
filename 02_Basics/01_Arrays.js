78c36704142de4a063667e136336e03b36df3d9f
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-12 14:51:16 +0000

    Almost done 🤝

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -4,5 +4,24 @@ const myArr = [0, 1, 2, 3, 4, 5];
 const heroes = ["Captain-America", "Soldier=Boy", "Miyamoto Musashi"];
 
 const myArr2 = new Array(1, 2, 3, 4);
+console.log(myArr2[1]);
 
-console.log(myArr[0]);
+// Array methods
+
+// myArr.push(6); //simply adds an element into the array
+// myArr.push(7); //simply adds an element into the array
+// myArr.pop(); //last value ko remove krdega
+
+// myArr.unshift(9); // inserts element in the beginning of the array.
+// myArr.shift(); // removes the first element
+
+
+console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
