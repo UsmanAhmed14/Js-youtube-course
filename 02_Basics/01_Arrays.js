224ebb85efee27e1809788174c0f605703a4b5d7
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-12 14:34:53 +0000

    MOVING ONTO METHODS

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -1,4 +1,8 @@
 // Array
 // @lang:javascript formatter
+const myArr = [0, 1, 2, 3, 4, 5];
+const heroes = ["Captain-America", "Soldier=Boy", "Miyamoto Musashi"];
 
-const heroes = [1, 2, 3, 4, 5, 6];
+const myArr2 = new Array(1, 2, 3, 4);
+
+console.log(myArr[0]);
