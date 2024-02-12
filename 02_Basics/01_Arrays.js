de3619de4a00214b6f0e9802b2d97efbb7e23f70
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-12 16:18:45 +0000

    Arrays 01 done

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -4,7 +4,7 @@ const myArr = [0, 1, 2, 3, 4, 5];
 const heroes = ["Captain-America", "Soldier=Boy", "Miyamoto Musashi"];
 
 const myArr2 = new Array(1, 2, 3, 4);
-console.log(myArr2[1]);
+// console.log(myArr2[1]);
 
 // Array methods
 
@@ -15,13 +15,25 @@ console.log(myArr2[1]);
 // myArr.unshift(9); // inserts element in the beginning of the array.
 // myArr.shift(); // removes the first element
 
-
-console.log(myArr);
+// console.log(myArr);
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-const newArr = myArr.join()
+// const newArr = myArr.join()
 
-console.log(myArr);
-console.log(newArr);
+// console.log(myArr);
+// console.log(newArr);
+
+// splice, slice
+
+console.log("A ", myArr);
+
+const myNewArray1 = myArr.slice(1, 3);
+
+console.log(myNewArray1);
+console.log("B ", myArr);
+
+const myNewArray2 = myArr.splice(1, 3);
+console.log("C ", myArr);
+console.log(myNewArray2);
