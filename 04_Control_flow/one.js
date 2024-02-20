244b440f701b761c261dfe42a0a34e32e97dfd56
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-20 17:42:03 +0000

    committing just for safety

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,23 @@
+// if statement
+
+const isLoggedIn = true;
+const temprature = 41;
+
+// if (temprature === 40) {
+//   console.log("Less than 50");
+// } else {
+//   console.log("Temprature is greater than 50");
+// }
+
+// <, >, <=, >=, ==(single ' = ' means ASSIGNMENT) when we use double ' = ' we check the equality, !=(not equal to), ===(checks datatypes as well that's why is called strict equal)
+
+// const score = 200;
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`User power ${power}`);
+// }
+// console.log(`User power ${power}`);
+
+const balance = 1000;
+
+if (balance > 500) console.log("test"), console.log("test2");
