2f99857dd6e33d562944351a1d5df9c164ae6789
Usman Ahmed <102541370+UsmanAhmed14@users.noreply.github.com>
2024-02-21 07:49:46 +0000

    Do this.... whiie(Condition)

---
+++ 05_Iterations/two.js
@@ -0,0 +1,23 @@
+// While Loop
+
+let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of Index is: ${index}`);
+//   index = index + 2;
+// }
+
+let myArray = ["flash", "superman", "batman"];
+let arr = 0;
+while (arr < myArray.length) {
+//   console.log(`Value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+// Do while loop (💡 first works then checks the condition)
+
+let score = 11;
+
+do {
+  console.log(`Score is: ${score}`);
+  score++;
+} while (score <= 10);
